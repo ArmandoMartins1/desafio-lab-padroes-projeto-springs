c059a970e663b40e3d6e73a5226f3a857e006ffd
ArmandoMartins1 <armando.amds97@gmail.com>
2024-08-08 19:18:03 -0300

    desafio de projeto att

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -0,0 +1,65 @@
+package one.digitalinnovation.gof.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.service.ClienteService;
+
+/**
+ * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
+ * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
+ * interface simples e coesa (API REST).
+ * 
+ * @author falvojr
+ */
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping
+	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+		return ResponseEntity.ok(clienteService.buscarTodos());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+		return ResponseEntity.ok(clienteService.buscarPorId(id));
+	}
+	//Pesquisar insercoes por nome
+	@GetMapping("/search")
+	public ResponseEntity<Iterable<Cliente>> buscarPorNome(@RequestParam String nome) {
+		Iterable<Cliente> clientes = clienteService.buscarPorNome(nome);
+		return ResponseEntity.ok(clientes);
+	}
+
+	//Pesquisar insercoes por cep
+	@GetMapping("/searchByCep")
+	public ResponseEntity<Iterable<Cliente>> buscarPorCep(@RequestParam String cep) {
+		Iterable<Cliente> clientes = clienteService.buscarPorCep(cep);
+		return ResponseEntity.ok(clientes);
+	}
+
+	@PostMapping
+	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+		clienteService.inserir(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		clienteService.atualizar(id, cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		clienteService.deletar(id);
+		return ResponseEntity.ok().build();
+	}
+
+}
